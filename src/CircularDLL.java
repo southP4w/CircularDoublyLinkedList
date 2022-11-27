fbcc8e843e63e09088337118210548d0cef0894d
southpaw <dangargiullo@gmail.com>
2022-11-27 08:51:46 -0500

    Add constructor, insertion, and rotation methods to CircularDLL.java and add tests to Main.java.

--- src/CircularDLL.java
+++ src/CircularDLL.java
@@ -1,12 +1,66 @@
-class Node {
+class Node
+{
 	int data;
 	Node next, prev;
+
+	public Node(int data) {
+		this.data = data;
+	}
 }
 
-public class CircularDLL {
-	Node head, tail;
+public class CircularDLL
+{
+	private Node head, tail;
+	private int size;
 
 	public CircularDLL() {
+		size = 0;
 		head = tail = null;
+	}
+
+	public void insertAtTail(int data) {
+		Node temp = new Node(data);
+		if (isEmpty())
+			head = temp;
+		else {
+			tail.next = temp;
+			temp.prev = tail;
+		}
+		tail = temp;
+		head.prev = tail;
+		tail.next = head;
+		size++;
+		temp = null;
+	}
+
+	public void rotate(int n) {
+		if (isEmpty() || n <= 0)
+			return;
+		for (int i = 0; i < n; i++)
+			head = head.next;
+	}
+
+	public void rotateBackwards(int n) {
+		if (isEmpty() || n <= 0)
+			return;
+		for (int i = 0; i < n; i++)
+			head = head.prev;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder().append('{');
+		Node current = head;
+		while (current.next != head) {
+			sb.append(current.data).append(", ");
+			current = current.next;
+		}
+		sb.append(current.data).append('}');
+
+		return sb.toString();
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+public class Main
+{
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		CircularDLL cdll = new CircularDLL();
+		cdll.insertAtTail(5);
+		cdll.insertAtTail(4);
+		cdll.insertAtTail(3);
+		cdll.insertAtTail(2);
+		cdll.insertAtTail(17);
+		cdll.insertAtTail(9);
+		System.out.println("ORIGINAL:");
+		System.out.println(cdll.toString() + '\n');
+		System.out.println("Rotating Forward twice:");
+		cdll.rotate(2);
+		System.out.println(cdll.toString() + '\n');
+		System.out.println("Rotating Backwards three times:");
+		cdll.rotateBackwards(3);
+		System.out.println(cdll.toString());
 	}
 }
